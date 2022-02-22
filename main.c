2b70b9f3586e07ba20ce28625a094e71ad02d3e6
Amit Khatri <amit.vkhatri@gmail.com>
2022-02-22 15:31:46 -0600

    Added file i/o

--- main.c
+++ main.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-const char* FILENAME = ".td";
+#define FILENAME ".todo"
+#define MAX_LINE_LENGTH 60
 
 bool todo_exists(const char* filename);
 void create_todo_list(const char* filename);
-void print_todo_list(const char* filename);
+void print_todo_list(const char* filename,int lim);
+void add_todo(const char* filename, char* line, int lim);
 
 int main(int argc, char* argv[]){
  	
 	//Look into getops for parsing input args
 
+
 	if(todo_exists(FILENAME)){
-		print_todo_list(FILENAME);
+		print_todo_list(FILENAME,MAX_LINE_LENGTH);
 	}
 	else{
 		printf("Todo file doesn't exist. Creating....\n");
@@ -21,7 +24,7 @@ int main(int argc, char* argv[]){
 	}	
 
 
-
+	
 
 
 	return 0;
@@ -50,7 +53,7 @@ void create_todo_list(const char* filename){
 }
 
 
-void print_todo_list(const char* filename){
+void print_todo_list(const char* filename,int lim){
 
 	/*
 	1. Open File
@@ -58,15 +61,38 @@ void print_todo_list(const char* filename){
 	3. Else print the todo list to stdout
 	*/
 	FILE *fp;
+	char line[lim];
 	int c;
 
 	fp = fopen(filename,"r");
 	c = fgetc(fp);
+
 	if(c == EOF){
 		printf("NO TODOS \n");
+		return;
 	}
-	else{
-		//Print todo list
-	}
+
+	ungetc(c,fp);
+
+	while(fgets(line,lim,fp) != NULL)
+		printf("%s",line);
+
+	fclose(fp);
+
 	return;
+}
+
+
+void add_todo(const char* filename, char* line, int lim){
+
+	FILE *fp;
+
+	fp = fopen(filename,"a");
+	fputs(line,fp);
+
+	fclose(fp);
+
+	return;
+
+
 }
