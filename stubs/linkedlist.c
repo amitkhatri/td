a3805c8495418402a003ba839c44cb05fecaa95d
Amit Khatri <amit.vkhatri@gmail.com>
2022-02-22 18:40:34 -0600

    Created stub for linked list

---
+++ stubs/linkedlist.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+	char *data;
+	int index;
+	struct Node* next;
+};
+
+int main(void){
+	
+	struct Node* head = NULL;
+	struct Node* second = NULL;
+	struct Node* third = NULL;
+
+	head = (struct Node*)malloc(sizeof(struct Node));
+	second = (struct Node*)malloc(sizeof(struct Node));
+	third = (struct Node*)malloc(sizeof(struct Node));
+
+	head -> index = 1;
+	head -> data = "FUCK";
+	head -> next = second;
+
+
+	second -> index = 2;
+	second -> data = "YOU";
+	second -> next = third;
+
+	third -> index = 3;
+	third -> data = "TOO";
+	third -> next = NULL;
+
+	struct Node* n = head;
+
+	while(n != NULL){
+		printf("%d: %s\n",n -> index, n -> data);
+		n = n -> next;
+	}
+
+	return 0;
+}
